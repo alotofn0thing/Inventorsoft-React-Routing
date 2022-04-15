285c3bda558a3de784b0d82185d963f194035d81
Volodymyr Shum <shum.vr@gmail.com>
2022-04-15 04:25:30 +0300

    Fix problem with active page

--- src/components/CustomLink.jsx
+++ src/components/CustomLink.jsx
@@ -3,7 +3,10 @@ import { Link, useMatch } from 'react-router-dom';
 import React from 'react';
 
 const CustomLink = ({ children, to, ...props }) => {
-	const match = useMatch(to);
+	const match = useMatch({
+		path: to,
+		end: to.length === 1,
+	});
 
 	return (
 		<Link to={to} style={{ color: match ? '#0f7dc6' : 'white' }} {...props}>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,5 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { CustomLink } from './CustomLink';
-import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../hook/useAuth';
 
 export const Layout = () => {
